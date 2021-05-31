9eb99eab144271d9ac979de789e54f40a989fdf0
Agung Prayogi <58617982+agungprayogi291@users.noreply.github.com>
2021-05-31 11:20:43 +0700

    Update ApiController.php
    Create, delet, read,update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,10 +4,25 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
+$successStatus =201;
   Public function getData()
-{
-  $category = Category::all();
-  Return response()->json([]
-}
+  {
+     $category = Category::all();
+     return response()->json(['success' => $category], $this->successStatus);
+  }
+  Public function createData(Request $request,Category $category)
+  {
+   $data = $request->all();
+   $data['is_publish'] = true;
+   $category->create($data);
+   return response()->json(['success'] => $data], $this->successStatus);
+
+   
+  }
+  Public function destroy(Category $category)
+  {
+   $data = $category->delete();
+    return response()->json(['success'=> $data], $this->successStatus);
+  }
 
 }

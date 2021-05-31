9481e3dce569791a1235d3d726a1216c5a2a9d65
Agung Prayogi <58617982+agungprayogi291@users.noreply.github.com>
2021-05-31 11:09:31 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,1 +1,13 @@
+namespace App\Http\Controllers;
+use App\Category;
+use Illuminate\Http\Request;
 
+class ApiController extends Controller
+{
+  Public function getData()
+{
+  $category = Category::all();
+  Return response()->json([]
+}
+
+}
